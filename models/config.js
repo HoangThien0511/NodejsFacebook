b133d510b7e2472a7e3e4448f8c2e29f60ccf67a
Ken Nguyen <kennj.consultant@gmail.com>
2024-03-14 11:08:41 +0700

    first commit

---
+++ models/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '249111854929838',
+        clientSecret: 'ff9fb129c07894da93b2c424db83e24d'
+    }
+
+}
